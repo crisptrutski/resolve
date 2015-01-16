e7ba1c0d78b67aae36d37f687ae545a25b8cfb35
Chris Truter <jeffpalentine@gmail.com>
2015-01-16 18:30:25 +0200

    Reorganise deps slightly

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [org.clojure/tools.namespace "0.2.8"]
                  [com.taoensso/carmine "2.9.0"]
                  [compojure "1.3.1"]
                  [environ "1.0.0"]
-                 [bss/rampant "0.1.0-SNAPSHOT"]
                  [ring "1.3.2"]
                  [fogus/ring-edn "0.2.0"]
-                 [tailrecursion/ring-proxy "2.0.0-SNAPSHOT"]]
+                 [tailrecursion/ring-proxy "2.0.0-SNAPSHOT"]
+
+                 [bss/rampant "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-environ "1.0.0"]]
 
@@ -21,6 +21,7 @@
   :uberjar-name "resolve.jar"
 
   :profiles {:dev {:repl-options {:init-ns bss.resolve.core}
+                   :dependencies [[org.clojure/tools.namespace "0.2.8"]]
                    :env {:is-dev true}}
 
              :prod {:jvm-opts ["-Xmx1g" "-server"] }
