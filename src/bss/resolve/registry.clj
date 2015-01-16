fcf4d9379d11b6b2d90806d4a7af9f902ff88423
Chris Truter <jeffpalentine@gmail.com>
2015-01-16 13:32:33 +0200

    More expression

--- src/bss/resolve/registry.clj
+++ src/bss/resolve/registry.clj
@@ -9,19 +9,37 @@
 (defn- create-entry [host port]
   {:host host, :port port})
 
-(defn exists? [service-name version host port]
-  (some
-   #{(create-entry host port)}
-   (get-in @registry [service-name (normalize version)])))
+(defn exists?
+  "Determines whether there are any matches, with match-all for missing params"
+  ([service-name]
+   (contains? @registry service-name))
+  ;; TODO: this should allow caret and wildcard ranges
+  ([service-name version]
+   (contains? (get @registry service-name) version))
+  ([service-name version host]
+   (some (comp #{host} :host)
+         (get-in @registry [service-name (normalize version)])))
+  ([service-name version host port]
+   (some #{(create-entry host port)}
+         (get-in @registry [service-name (normalize version)]))))
 
 ;; gracefully handle already-existing (no-op)
-(defn register [service-name version host port]
+(defn register
+  "Deregister {host,port} as an endpoint offering service@version"
+  [service-name version host port]
   (swap! registry update-in
          [service-name (normalize version)]
          #(conj (set %) (create-entry host port))))
 
 ;; gracefully handle non-existing (no-op)
-(defn unregister [service-name version host port]
+(defn unregister
+  "Reverse `register`"
+  [service-name version host port]
   (swap! registry update-in
          [service-name (normalize version)]
          #(disj % (create-entry host port))))
+
+(defn versions-for
+  "Determines set of versions available for given service-name"
+  [service-name]
+  (set (keys (get @registry service-name))))
--- test/bss/resolve/registry_test.clj
+++ test/bss/resolve/registry_test.clj
@@ -16,7 +16,6 @@
       (apply unregister args)
       (is (not (apply exists? args))))))
 
-
 (deftest graceful-test
   (testing "it silently no-ops on redundant calls"
     (setup)
@@ -29,3 +28,26 @@
       (apply unregister args)
       (apply unregister args)
       (is (not (apply exists? args))))))
+
+;; TODO: SHOULD SUPPORT VERSION WILDCARDS
+(deftest exists?-test
+  (testing "non-precise matches"
+    (setup)
+    (register "a" "0.0.1" "cluster1" 8081)
+    (is (exists? "a"))
+    (is (not (exists? "b")))
+    (is (exists? "a" "0.0.1"))
+    (is (not (exists? "a" "0.0.2")))
+    (is (exists? "a" "0.0.1" "cluster1"))
+    (is (not (exists? "a" "0.0.1" "cluster2")))))
+
+(deftest version-for-test
+  (testing "basic expectations"
+    (setup)
+    (register "a" "0.0.1" "cluster1" 8080)
+    (register "a" "0.1.0" "cluster2" 8080)
+    (register "b" "1.0.1" "cluster1" 8081)
+    (register "b" "1.0.2" "cluster2" 8081)
+    (register "b" "1.1.0" "cluster1" 8082)
+    (is (= #{"0.0.1" "0.1.0"} (versions-for "a")))
+    (is (= #{"1.0.1" "1.0.2" "1.1.0"} (versions-for "b")))))
