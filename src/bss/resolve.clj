4964c80aed007a9379d74b2a35943e4617a6b375
Chris Truter <jeffpalentine@gmail.com>
2015-01-16 10:20:17 +0200

    Lein new

---
+++ src/bss/resolve.clj
@@ -0,0 +1,6 @@
+(ns bss.resolve)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/bss/resolve_test.clj
@@ -0,0 +1,7 @@
+(ns bss.resolve-test
+  (:require [clojure.test :refer :all]
+            [bss.resolve :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
