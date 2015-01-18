42decc3f23aa4e7f4cbb586ed161fec645ee58d7
Chris Truter <jeffpalentine@gmail.com>
2015-01-18 23:50:03 +0200

    Basic client-usage namespace, WIP
    The async interface does not yet support waiting yet, throws
    exception to make this abundantly clear.
    
    Closes #8

---
+++ src/bss/resolve/client.clj
@@ -0,0 +1,32 @@
+(ns bss.resolve.client
+  (:require #_[clj-http.client :refer [request]]
+            [bss.resolve.registry :as reg]))
+
+(defn- rand-set
+  "Pick random element from set"
+  [set]
+  (-> set seq rand-nth))
+
+(defn url-for
+  "Synchronous, provides a endpoint from matched set (if any exist), otherwise
+  provide a proxying route"
+  [registry service-name version]
+  (or
+   ;; TODO: should possibly allow passing metadata used to support stickyness, etc
+   ;;       and defer to specific strategy for choosing endpoint, rather than rand-set
+   (rand-set (reg/lookup registry service-name version))
+   ;; TODO should provide registry base URI here (via config?)
+   (str "/proxy/" service-name "/" version)))
+
+(defn with-url
+  "Asynchronous version, trades performances for robustness.
+  Still no guarantee - endpoint can go down any time after delivery"
+  [registry service-name version]
+  (let [p (promise)]
+    (if-let [e (rand-set (reg/lookup registry service-name version))]
+      (deliver p e)
+      ;; TODO: deliver as soon as a service that supports this [name, version] is
+      ;;       registered. this requires adding some more state to registries.
+      ;;       eg. (reg/register-promise registry service-name version p)
+      (throw (UnsupportedOperationException. "Not implemented yet")))
+    p))
