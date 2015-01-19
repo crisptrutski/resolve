5f49e11a107525f5032141bdad5788429404e0ed
Chris Truter <jeffpalentine@gmail.com>
2015-01-20 01:06:55 +0200

    Auto-register component for services
    Convenience is coming, this is a bit rought right now though
    
    (see backend for an example using it)

--- src/bss/resolve/client.clj
+++ src/bss/resolve/client.clj
@@ -1,6 +1,8 @@
 (ns bss.resolve.client
   (:require #_[clj-http.client :refer [request]]
-            [bss.resolve.registry :as reg]))
+            [bss.resolve.redis-registry :as rr]
+            [bss.resolve.registry :as reg]
+            [com.stuartsierra.component :as component]))
 
 (defn- rand-set
   "Pick random element from set"
@@ -30,3 +32,52 @@
       ;;       eg. (reg/register-promise registry service-name version p)
       (throw (UnsupportedOperationException. "Not implemented yet")))
     p))
+
+
+;; TODO: should really be a lightweight peer (see gossip story)
+;;       for now just exposing redis conn string via API allows
+;;       this to create a syncing peer
+(defn get-registry [uri]
+  (component/start (rr/map->RedisRegistry {})))
+
+(defn register [{:keys [service-name
+                        service-registry
+                        host
+                        port
+                        version] :as config}]
+  (let [registry (get-registry service-registry)]
+    (if-not service-registry
+      (println "No service registry defined, not registering self")
+      (let [missing-keys (remove #(get config %) [:service-name :port :version])]
+        (println "Registering service")
+        (if (seq missing-keys)
+          (throw (RuntimeException. (str "Missing required keys: " (into [] missing-keys))))
+          (reg/register registry
+                        service-name
+                        version
+                        host
+                        port))))))
+
+(defn unregister [{:keys [service-name
+                          service-registry
+                          host
+                          port
+                          version] :as config}]
+  (let [registry (get-registry service-registry)]
+    (if-not service-registry
+      (println "No service registry defined, nothing to deregister")
+      (when (some #(get config %) [:service-name :port :version])
+        (println "Unregistering service")
+        (reg/unregister registry
+                        service-name
+                        version
+                        host
+                        port)))))
+
+(defrecord AutoRegistry [service-name service-registry host port version]
+  component/Lifecycle
+  (start [this] (register this) this)
+  (stop [this] (unregister this) this))
+
+(defn auto-registry [conf]
+  (map->AutoRegistry conf))
