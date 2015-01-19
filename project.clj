1db4bbe611c5301de32be3c1f47dc78fbca43296
Chris Truter <jeffpalentine@gmail.com>
2015-01-19 10:16:55 +0200

    Remove EPL

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject bss/resolve "0.1.0"
   :description "Service registry and proxy"
   :url "https://github.com/organosoft/resolve"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [com.taoensso/carmine "2.9.0"]
